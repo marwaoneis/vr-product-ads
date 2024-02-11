1d09ac0c1150876daab64c52ade152c5bb315dc6
marwaoneis <marwa.oneis22@gmail.com>
2024-02-11 19:09:13 +0200

    Added hero and header components to the landing screen

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+
+const Landing = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default Landing;
