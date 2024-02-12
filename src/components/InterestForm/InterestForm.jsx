02bf7faf48ec714ae543de350b3ed51218d2151c
marwaoneis <marwa.oneis22@gmail.com>
2024-02-12 14:45:51 +0200

    Implemented interest form section

--- src/components/InterestForm/InterestForm.jsx
+++ src/components/InterestForm/InterestForm.jsx
@@ -4,31 +4,31 @@ import "./style.css";
 const InterestForm = () => {
   return (
     <div className="interest-form-container">
-      <h2>INTERESTED IN VR ADS?</h2>
+      <h2 className="title">INTERESTED IN VR ADS?</h2>
       <p>Fill out a short form and we will call you back</p>
-      <form className="interest-form">
-        <input
-          type="text"
-          placeholder="Jessica Brown"
-          className="input-field"
-        />
-        <input
-          type="text"
-          placeholder="+40 842 82 21 129"
-          className="input-field"
-        />
-        <input
-          type="email"
-          placeholder="jessbr@gmail.com"
-          className="input-field"
-        />
+      <div className="left-ball"></div>
+      <div className="form-row">
+        <div className="input-container">
+          <input type="text" placeholder="Full Name" className="input-field" />
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="input-field"
+          />
+        </div>
+        <div className="input-container">
+          <input type="email" placeholder="Email" className="input-field" />
+        </div>
         <button type="submit" className="submit-button">
           Call me back
         </button>
-        <label className="privacy-policy">
-          <input type="checkbox" />I accept privacy policy
-        </label>
-      </form>
+      </div>
+      <label className="privacy-policy">
+        <input type="checkbox" />I accept privacy policy
+      </label>
+      <h2 className="try">WANT TO TRY?</h2>
     </div>
   );
 };
