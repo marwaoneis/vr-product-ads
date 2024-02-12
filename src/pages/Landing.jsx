450aa40591edc8956f4d337413149c0e1cb2f479
marwaoneis <marwa.oneis22@gmail.com>
2024-02-12 02:31:03 +0200

    Included interest form component in the landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,15 @@
+import AboutSection from "../components/About/About";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import InterestForm from "../components/InterestForm/InterestForm";
 
 const Landing = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <AboutSection />
+      <InterestForm />
     </div>
   );
 };
