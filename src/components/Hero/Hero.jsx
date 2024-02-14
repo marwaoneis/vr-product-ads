b0e2ac6194a31afc47330a7c29c7160da256b945
marwaoneis <marwa.oneis22@gmail.com>
2024-02-14 02:00:45 +0200

    Edited hero component parallax transitions

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./style.css";
-import heroImage from "../../assets/hero-image.png";
+import heroImage from "../../assets/hero-image1.png";
+import newImage from "../../assets/new-image.png";
 import diagonalSlash1 from "../../assets/slash1.svg";
 import diagonalSlash2 from "../../assets/slash2.svg";
 import diagonalSlash3 from "../../assets/slash3.svg";
@@ -10,95 +11,178 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+  const heroContainerRef = useRef();
+  const heroImageRef = useRef();
+  const heroContentRef = useRef();
+  const newImageRef = useRef();
+  const [overflowHidden, setOverflowHidden] = useState(false); // State to control overflow
+
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: ".hero-container",
+        trigger: heroContainerRef.current,
         start: "top top",
-        end: "bottom top",
-        scrub: true,
+        end: "200% top",
+        scrub: 1,
         pin: true,
-        anticipatePin: 1,
-        onUpdate: (self) => {
-          if (self.direction === -1) {
-            reverseAnimation();
-          }
-        },
+        onEnter: () => setOverflowHidden(true), // Hide overflow when entering
+        onLeaveBack: () => setOverflowHidden(false), // Show overflow when leaving back
       },
     });
 
-    // Forward animation
-    tl.to(".hero-image", {
-      x: "-50%",
-      scale: 3,
-      duration: 0.5,
-      ease: "power1.inOut",
-    })
+    tl.to(
+      ".triangle, .circle-right, .circle-center, .circle-left",
+      {
+        autoAlpha: 0,
+        ease: "none",
+      },
+      0
+    )
       .to(
-        ".hero-content",
+        heroContentRef.current,
         {
           xPercent: -100,
           autoAlpha: 0,
-          duration: 0.5,
-          ease: "power1.inOut",
+          ease: "none",
+        },
+        0
+      )
+      .to(
+        heroImageRef.current,
+        {
+          xPercent: -50, // Moves the image to the center
+          scale: 7, // Zooms in on the VR glasses
+          ease: "none",
+          transformOrigin: "50% 25%",
+        },
+        0
+      ) // Start at the same time as the text moves out
+      .to(
+        newImageRef.current,
+        {
+          autoAlpha: 1,
+          duration: 1,
+          ease: "none",
         },
         "<"
       )
       .to(
-        ".circle-left, .triangle",
+        ".triangle, .circle-right, .circle-center, .circle-left",
         {
-          y: 100,
-          duration: 0.5,
-          ease: "power1.inOut",
+          autoAlpha: 0,
+          ease: "none",
+          immediateRender: false, // Prevents immediate rendering when the component mounts
         },
-        "<"
+        ">"
       );
-
-    // Reverse animation
-    const reverseAnimation = () => {
-      const reverseTl = gsap.timeline();
-      reverseTl
-        .to(".hero-image", {
-          x: "0%",
-          scale: 1,
-          duration: 0.5,
-          ease: "power1.inOut",
-        })
-        .to(
-          ".hero-content",
-          {
-            xPercent: 0,
-            autoAlpha: 1,
-            duration: 0.5,
-            ease: "power1.inOut",
-          },
-          "<"
-        )
-        .to(
-          ".circle-left, .triangle",
-          {
-            y: 0,
-            duration: 0.5,
-            ease: "power1.inOut",
-          },
-          "<"
-        );
-    };
-
     return () => {
       ScrollTrigger.getAll().forEach((t) => t.kill());
+      setOverflowHidden(false); // Reset overflow when component is unmounted
     };
   }, []);
 
+  // useEffect(() => {
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ".hero-container",
+  //       start: "top top",
+  //       end: "bottom top",
+  //       scrub: true,
+  //       pin: true,
+  //       anticipatePin: 1,
+  //       onUpdate: (self) => {
+  //         if (self.direction === -1) {
+  //           reverseAnimation();
+  //         }
+  //       },
+  //     },
+  //   });
+
+  //   // Forward animation
+  //   tl.to(".hero-image", {
+  //     x: "-50%",
+  //     // scale: 3,
+  //     duration: 0.5,
+  //     ease: "power1.inOut",
+  //   }).to(
+  //     ".hero-content",
+  //     {
+  //       xPercent: -100,
+  //       autoAlpha: 0,
+  //       duration: 0.5,
+  //       ease: "power1.inOut",
+  //     },
+  //     "<"
+  //   );
+  //   // .to(
+  //   //   ".circle-left, .triangle",
+  //   //   {
+  //   //     y: 100,
+  //   //     duration: 0.5,
+  //   //     ease: "power1.inOut",
+  //   //   },
+  //   //   "<"
+  //   // );
+
+  //   // Reverse animation
+  //   const reverseAnimation = () => {
+  //     const reverseTl = gsap.timeline();
+  //     reverseTl
+  //       .to(".hero-image", {
+  //         x: "0%",
+  //         scale: 1,
+  //         duration: 0.5,
+  //         ease: "power1.inOut",
+  //       })
+  //       .to(
+  //         ".hero-content",
+  //         {
+  //           xPercent: 0,
+  //           autoAlpha: 1,
+  //           duration: 0.5,
+  //           ease: "power1.inOut",
+  //         },
+  //         "<"
+  //       );
+  //     // .to(
+  //     //   ".circle-left, .triangle",
+  //     //   {
+  //     //     y: 0,
+  //     //     duration: 0.5,
+  //     //     ease: "power1.inOut",
+  //     //   },
+  //     //   "<"
+  //     // );
+  //   };
+
+  //   return () => {
+  //     ScrollTrigger.getAll().forEach((t) => t.kill());
+  //   };
+  // }, []);
+
   return (
-    <div className="hero-container">
+    <div
+      ref={heroContainerRef}
+      className={`hero-container ${overflowHidden ? "overflow-hidden" : ""}`}
+    >
       <div className="triangle"></div>
-      <div className="hero-content">
+      <div ref={heroContentRef} className="hero-content">
         <img src={diagonalSlash1} alt="" class="diagonal-slash-1" />
         <h2 className="hero-main-text">Experience Ads</h2>
         <h2 className="hero-sub-text">in a New Dimension.</h2>
       </div>
-      <img src={heroImage} alt="VR Headset" className="hero-image" />
+      <img
+        ref={heroImageRef}
+        src={heroImage}
+        alt="VR Headset"
+        className="hero-image"
+      />
+      <img
+        ref={newImageRef}
+        src={newImage}
+        alt="New Image"
+        className="new-image"
+      />
       <img src={diagonalSlash2} alt="" class="diagonal-slash-2" />
       <img src={diagonalSlash3} alt="" class="diagonal-slash-3" />
       <div className="circle-right"></div>
